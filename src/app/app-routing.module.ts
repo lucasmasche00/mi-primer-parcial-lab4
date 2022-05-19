dc6dece8a1347b05ff48b5534069310133377c68
Lucas Martín Mascheroni <lucasmasche@hotmail.com>
2022-05-19 16:52:18 -0300

    Prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'bienvenido', pathMatch: 'full' },
+  { path: 'bienvenido', component: BienvenidoComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './componentes/main-menu/main-menu.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    NotFoundComponent,
+    BienvenidoComponent
   ],
   imports: [
     BrowserModule,
